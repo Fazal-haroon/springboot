87da379aa0ed08ae0d2884fa5f9dc3cfaaa34a14
Fazal Haroon <kharoon187@gmail.com>
2022-12-03 23:32:34 +0400

    complete crud operation without database

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -3,10 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.entity.Employee;
 import com.example.demo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import javax.validation.Valid;
@@ -18,13 +15,29 @@ public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
-    @RequestMapping("/employees")
-    public List<Employee> getAllEmployee(){
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployee() {
         return employeeService.getAllEmployee();
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "/employee")
-    public Employee getEmployeeById(@RequestParam(value = "id", required = true) Integer id){
+    @GetMapping(value = "/employee")
+    public Employee getEmployeeById(@RequestParam(value = "id", required = true) Integer id) {
         return employeeService.getEmployee(id);
+    }
+
+    @PostMapping(value = "/employee")
+    public void createEmployee(@RequestBody Employee employee) {
+        employeeService.createEmployee(employee);
+    }
+
+    @PutMapping(value = "/employee")
+    public void updateEmployee(@RequestParam Integer id, @RequestBody Employee employee) {
+        employeeService.updateEmployee(id, employee);
+    }
+
+    @DeleteMapping(value = "/employee/{id}")
+    public List<Employee> deleteEmployee(@PathVariable Integer id){
+        employeeService.deleteEmployee(id);
+        return employeeService.getAllEmployee();
     }
 }
