94193ac3fa1f3aa83cd2749780a732d0ebca30ce
Fazal Haroon <kharoon187@gmail.com>
2022-12-06 19:04:40 +0400

    added new thing thymeleaf

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class MainController {
     @GetMapping("/home")
@@ -16,7 +19,20 @@ public class MainController {
 
     @GetMapping("/ternary")
     public String elvisExample(Model model){
-        model.addAttribute("isAdmin", true);
+        /**
+         * elvis operator ?: same like ternary operator
+         * if and unless
+         * switch case
+         */
+        model.addAttribute("isAdmin", false);
+        model.addAttribute("gender", "F");
         return "elvis";
+    }
+
+    @GetMapping("/each")
+    public String eachExample(Model model){
+        List<String> stringList = Arrays.asList("First", "Second", "Third");
+        model.addAttribute("list", stringList);
+        return "each";
     }
 }
