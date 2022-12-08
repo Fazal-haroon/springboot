15a38de583d1e8a2c956215b9da7c761caac4b31
Fazal Haroon <kharoon187@gmail.com>
2022-12-08 14:36:44 +0400

    defined RunnableExample with traditional and lambda way

---
+++ src/main/java/com/example/demo/section1/classd/RunnableExample.java
@@ -0,0 +1,37 @@
+package com.example.demo.section1.classd;
+
+public class RunnableExample {
+    public static void main(String[] args) {
+        //Runnable Traditional Example
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                int sum = 0;
+                for (int i=0; i<10; i++){
+                    sum += i;
+                }
+                System.out.println("Runnable Traditional : "+sum);
+            }
+        };
+        new Thread(runnable).start();
+
+        //Implement using lambda
+        Runnable runnable1 = () -> {
+            int sum = 0;
+            for (int i = 0; i < 10; i++) {
+                sum += i;
+            }
+            System.out.println("Runnable Lambda : "+sum);
+        };
+        new Thread(runnable1).start();
+
+        //Implement using Thread with Lambda
+        new Thread(() -> {
+            int sum = 0;
+            for (int i = 0; i < 10; i++) {
+                sum += i;
+            }
+            System.out.println("Runnable Thread Lambda : "+sum);
+        }).start();
+    }
+}
