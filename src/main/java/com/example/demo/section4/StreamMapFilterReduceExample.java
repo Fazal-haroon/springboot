d6852c4d371fbe7d9a8f483002112dc6b34b3e12
Fazal Haroon <kharoon187@gmail.com>
2022-12-10 15:20:51 +0400

    min, limit, skip, findFirst, findAny added

--- src/main/java/com/example/demo/section4/StreamMapFilterReduceExample.java
+++ src/main/java/com/example/demo/section4/StreamMapFilterReduceExample.java
@@ -3,6 +3,7 @@ package com.example.demo.section4;
 import com.example.demo.section2.classd.Instructor;
 import com.example.demo.section2.classd.Instructors;
 
+import java.util.List;
 import java.util.Optional;
 
 public class StreamMapFilterReduceExample {
@@ -16,5 +17,26 @@ public class StreamMapFilterReduceExample {
         System.out.println("result3 = " + result3.get());
         Optional<Integer> result4 = Instructors.getAll().stream().filter(Instructor::isOnlineCourses).map(Instructor::getYearsOfExpereience).reduce(Integer::sum);
         System.out.println("result4 = " + result4.get());
+
+        //max using stream max function
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 11, 22, 9);
+        Optional maxResult = numbers.stream().max(Integer::compareTo);
+        if (maxResult.isPresent()) {
+            System.out.println("maxResult = " + maxResult.get());
+        }
+        //(0,1) - 1
+        //(1,2) - 2
+        //(2,3) - 3
+        int maxResult2 = numbers.stream().reduce(0, (a, b) -> a > b ? a : b);
+        System.out.println("maxResult2 = " + maxResult2);
+        Optional maxResult3 = numbers.stream().reduce((a, b) -> a > b ? a : b);
+        if (maxResult3.isPresent()) {
+            System.out.println("maxResult3.get() = " + maxResult3.get());
+        }
+
+        Optional maxResult4 = numbers.stream().reduce(Integer::max);
+        if (maxResult4.isPresent()) {
+            System.out.println("maxResult4.get() = " + maxResult4.get());
+        }
     }
 }
