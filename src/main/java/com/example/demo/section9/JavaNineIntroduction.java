f930d254e2ee21341207734d5eae58050cf21643
Fazal Haroon <kharoon187@gmail.com>
2022-12-15 12:16:07 +0400

    Java 9 introduction and how to use jshell in CMD terminal

---
+++ src/main/java/com/example/demo/section9/JavaNineIntroduction.java
@@ -0,0 +1,41 @@
+package com.example.demo.section9;
+
+public class JavaNineIntroduction {
+    /*
+    * when learning any programming language immediate feedback is important and with traditional java programmming this was missing since the beginning of java
+    *
+    * so with java 9, JShell was introduced to fill this gap. JShell is an interactive tool for learning java nad prototyping java code
+    *
+    * JShell which is a Read-Evaluate-Print-Loop (REPL) can help flatten out the learning curve of java
+    *
+    * it evaluates declarations, statements, and expressions as they are entered and immediately shows the results
+    *
+    * the tool is run from the command line
+    *
+    * Java has a step learning curve, as you know just to print out the "hello world" you have to type in 5 line of code.
+    *
+    * public class Main {
+    *       public static void main(String args[]{
+    *           System.out.println("Hello World");
+    *       }
+    * }
+    *
+    * but in REPL you can print "hello world" by simply typing in the
+    * System.out.println("Hello World");
+    * No import, no package, no class just above command(statement) and also without any semicolon
+    *
+    * Many many programming languages have REPL such as python, scala, swift, javascript and many others
+    *
+    * REPL supports the statements, expressions, declaration, interface, method declaration, field declaration, import declarations and external libraries
+    *
+    * REPL is used to writting and testing small code snippets, REPL is not a integrated development envnironment and they are not used to write programs of any significant size
+    *
+    * 1. check the 1jshell.png image in jshellJava9Introduction folder
+    * 2. adding two numbers 2jshell.png
+    * 3. to clear the shell hit {ctrl + L}
+    * 4. int initialize with some number and multiple by 3 in 3shell.png
+    * 5. check string example in 4shell.png
+    * 6. by default import in jshell 5jshell.png
+    * 7.
+    * */
+}
