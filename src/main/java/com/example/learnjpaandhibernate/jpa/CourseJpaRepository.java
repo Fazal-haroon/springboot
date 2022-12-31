9cac1ed4319a8fe77130af68fe84ff1e9072dbd9
Fazal Haroon <kharoon187@gmail.com>
2022-12-31 13:15:50 +0400

    we graduated from spring JDBC to JPA

---
+++ src/main/java/com/example/learnjpaandhibernate/jpa/CourseJpaRepository.java
@@ -0,0 +1,30 @@
+package com.example.learnjpaandhibernate.jpa;
+
+import com.example.learnjpaandhibernate.entity.Course;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Transactional
+public class CourseJpaRepository {
+
+    @PersistenceContext
+//    @Autowired //--> instead of autowired, we are using a more specific annotation called a persistent context.
+    private EntityManager entityManager;
+
+    public void insert(Course course){
+        entityManager.merge(course);
+    }
+
+    public Course findById(long id){
+        return entityManager.find(Course.class, id);
+    }
+
+    public void deleteById(long id){
+        Course course = entityManager.find(Course.class, id);
+        entityManager.remove(course);
+    }
+
+}
